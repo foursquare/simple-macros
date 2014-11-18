70d016520d0ff1ba9c08b4e996112a5f91966cdd
John Gallagher <john@foursquare.com>
2014-11-18 13:33:44 -0500

    Initial gh-pages commit

---
+++ api/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"com" : [], "com.foursquare" : [], "com.foursquare.macros" : [{"object" : "com\/foursquare\/macros\/CodeRef$.html", "case class" : "com\/foursquare\/macros\/CodeRef.html", "name" : "com.foursquare.macros.CodeRef"}]};
