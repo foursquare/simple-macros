d929a408d302706511f6ec6b7885e95a0748fc4f
John Gallagher <john@foursquare.com>
2014-11-17 19:46:53 -0500

    CodeRef: add docstrings

--- src/main/scala/com/foursquare/macros/CodeRef.scala
+++ src/main/scala/com/foursquare/macros/CodeRef.scala
@@ -6,41 +6,71 @@ import java.io.File
 import language.experimental.macros
 import scala.reflect.macros.Context
 
+
+/**
+ * Holder for file relative to the compiler
+ * working directory as a string, and line number
+ * of file. Created explicitly with [[com.foursquare.macros.CodeRef.CODEREF]]
+ * or implicitly with [[com.foursquare.macros.CodeRef.materializeCodeRef]]
+ */
 case class CodeRef(val file: String, val line: Int)
 
 object CodeRef {
 
-  // CODEREF: This macro provides a case class with the file and line number
-  //          It is convenient to use when passing the values into a function
-  //          since you get type safety
+  /**
+   * Explicit macro call provides a case class with the file and line number
+   * {{{
+   *  val here = CODEREF
+   * }}}
+   */
   def CODEREF: CodeRef = macro codeRefImpl
 
+  /**
+   * Explicit macro call provides the line number
+   * {{{
+   *  val here = LINE
+   * }}}
+   */
+  def LINE: Int = macro lineImpl
+
+  /**
+   * Explicit macro call provides the file relative to the compiler
+   * working directory as a string
+   * {{{
+   *  val here = FILE
+   * }}}
+   */
+  def FILE: String = macro fileImpl
+
+  /**
+   * Implicit macro definiton provides a case class with the file and line number
+   * useful for passing as an implicit reference to methods
+   * {{{
+   *   def foo(bar: Int)(implicit caller: CodeRef) {
+   *     println("called foo(" + bar + ") at " + caller)
+   *   }
+   * }}}
+   */
+  implicit def materializeCodeRef: CodeRef = macro codeRefImpl
+
   def codeRefImpl(c: Context): c.Expr[CodeRef] = {
-    import c.universe._
+    import c.universe.reify
 
     val file = fileImpl(c)
     val line = lineImpl(c)
     reify { com.foursquare.macros.CodeRef(file.splice, line.splice) }
   }
 
-  implicit def materializeCodeRef: CodeRef = macro codeRefImpl
-
-  // CODEREF: Macro for line number as an Int
-  def LINE: Int = macro lineImpl
-
   def lineImpl(c: Context): c.Expr[Int] = {
-    import c.universe._
+    import c.universe.{Constant, Expr, Literal}
 
     val line = Literal(Constant(c.enclosingPosition.line))
 
     c.Expr[Int](line)
   }
 
-  // FILE: Macro for file name relative to working directory as a string
-  def FILE: String = macro fileImpl
-
   def fileImpl(c: Context): c.Expr[String] = {
-    import c.universe._
+    import c.universe.{Constant, Expr, Literal}
 
     val absolute = c.enclosingPosition.source.file.file.toURI
     val base = new File(".").toURI
@@ -49,5 +79,4 @@ object CodeRef {
 
     c.Expr[String](path)
   }
-
 }
